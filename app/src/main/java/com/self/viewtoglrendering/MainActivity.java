ddb4252be77cdc9290782445ff426bd499965142
kurt.su <sudao2000@126.com>
2017-03-07 18:23:46 +0800

    support lottie

--- app/src/main/java/com/self/viewtoglrendering/MainActivity.java
+++ app/src/main/java/com/self/viewtoglrendering/MainActivity.java
@@ -29,16 +29,16 @@ public class MainActivity extends ActionBarActivity {
 
         mGLSurfaceView = (GLSurfaceView) findViewById(R.id.gl_surface_view);
         mGLLinearLayout = (GLRenderable) findViewById(R.id.gl_layout);
-        mWebView = (WebView) findViewById(R.id.web_view);
+        //mWebView = (WebView) findViewById(R.id.web_view);
 
         mGLSurfaceView.setEGLContextClientVersion(2);
         mGLSurfaceView.setRenderer(viewToGlRenderer);
 
         mGLLinearLayout.setViewToGLRenderer(viewToGlRenderer);
 
-        mWebView.setWebViewClient(new WebViewClient());
-        mWebView.setWebChromeClient(new WebChromeClient());
-        mWebView.loadUrl("http://stackoverflow.com/questions/12499396/is-it-possible-to-render-an-android-view-to-an-opengl-fbo-or-texture");
+        //mWebView.setWebViewClient(new WebViewClient());
+        //mWebView.setWebChromeClient(new WebChromeClient());
+        //mWebView.loadUrl("http://stackoverflow.com/questions/12499396/is-it-possible-to-render-an-android-view-to-an-opengl-fbo-or-texture");
     }
 
 
